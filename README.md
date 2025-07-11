c4da996308d95c1e4949e7680e87aa99559bab92
xu9010 <xu9010@gmail.com>
2025-07-11 21:15:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# gfwlist2pac
+每天20:45(UTC)，4:45(UTC+8)自动更新
+
+用户规则：user-rule.txt
+
+https://raw.githubusercontent.com/xu9010/gfwlist2pac/master/gfwlist_1081.pac
+https://raw.githubusercontent.com/xu9010/gfwlist2pac/master/gfwlist_7890.pac
---
+++ update.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+cd `dirname $0`
+git reset --hard
+git pull
+
+
+rm -rf env
+virtualenv env
+source env/bin/activate
+
+# 固定使用3.0.1版
+(cd genpac-a390426bdf6b36ac7abb78e32819cd93fb41e619; python setup.py install) 
+# (cd genpac; git checkout a390426; python setup.py install) # 3.0.1版
+# pip install genpac==3.0.1 # 备选在线安装
+
+wget -O gfwlist/gfwlist.txt https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
+
+env/bin/genpac \
+	--format pac \
+	--pac-proxy "SOCKS5 127.0.0.1:1081" \
+	--gfwlist-url - \
+	--gfwlist-local gfwlist/gfwlist.txt \
+	--user-rule-from user-rule.txt \
+	-o gfwlist_1081.pac
+sed -e '3d' -i gfwlist_1081.pac # 删除带无用日期的注释
+
+
+env/bin/genpac \
+	--format pac \
+	--pac-proxy "SOCKS5 127.0.0.1:7890" \
+	--gfwlist-url - \
+	--gfwlist-local gfwlist/gfwlist.txt \
+	--user-rule-from user-rule.txt \
+	-o gfwlist_7890.pac
+sed -e '3d' -i gfwlist_1081.pac # 删除带无用日期的注释
+
+deactivate
+
+git add .
+git commit -m "[$(LANG=C date)]auto update"
+git push -f origin master
